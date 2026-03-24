28cdfa0717fd11dd3cd99a04ea00a893510b25c2
maks_kras <maksym_krasovskiy@ya.ru>
2026-03-24 15:02:06 +0300

    add a calculated field lessons_count for CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -4,12 +4,26 @@ from courses.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Course.
+    (Добавляет вычисляемое поле lessons_count, которое возвращает количество
+    уроков, связанных с курсом)
+    """
+
+    lessons_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
 
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков для данного курса"""
+        return obj.lessons.count()
+
 
 class LessonsSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Lesson"""
+
     class Meta:
         model = Lesson
         fields = "__all__"
