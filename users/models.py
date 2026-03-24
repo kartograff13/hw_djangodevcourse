0e6173158811adbcd189585b1415c5781eea563a
maks_kras <maksym_krasovskiy@ya.ru>
2026-03-24 16:32:24 +0300

    add model for users/models.py CustomUserManager for createsuperuser

--- users/models.py
+++ users/models.py
@@ -1,8 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 from courses.models import Course, Lesson
+
+
+class CustomUserManager(BaseUserManager):
+    """Менеджер для модели User с авторизацией по email"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email должен быть указан.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -20,6 +45,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = "Пользователь"
