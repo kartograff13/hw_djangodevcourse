6a51e77737760d12e79124567639b02c75a6d70d
maks_kras <maksym_krasovskiy@ya.ru>
2026-04-01 11:49:27 +0300

    add the Subscription model in courses/models.py + makemigrations&migrate

--- courses/models.py
+++ courses/models.py
@@ -50,3 +50,23 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Subscription(models.Model):
+    """Модель подписки пользователя на обновлении курса"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subscriptions", verbose_name="Пользователь"
+    )
+
+    course = models.ForeignKey("Course", on_delete=models.CASCADE, related_name="subscriptions", verbose_name="Курс")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        unique_together = ("user", "course")
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+    def __str__(self):
+        return f"{self.user.email} -> {self.course.title}"
