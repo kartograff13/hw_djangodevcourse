9cd023bed231bb1f6432b09e9b8adf57d431f442
maks_kras <maksym_krasovskiy@ya.ru>
2026-04-11 19:41:39 +0300

    add last_notification_sent for Course (model) + makemigrations&migrate

--- courses/models.py
+++ courses/models.py
@@ -17,6 +17,9 @@ class Course(models.Model):
         null=True,
     )
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Цена")
+    last_notification_sent = models.DateTimeField(
+        blank=True, null=True, verbose_name="Дата последней отправки уведомления об обновлении"
+    )
 
     class Meta:
         verbose_name = "Курс"
